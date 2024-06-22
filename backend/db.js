72d73ea2b3b7402517c302fef2f65086ba9cd2a1
Matheus Goulart <goulart77matheus@gmail.com>
2024-06-22 01:09:06 -0300

    Prod Banco

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
+    host: "195.200.4.201",
+    user: "splash-admin",
     password: "sazu",
     database: "splash"
 })
