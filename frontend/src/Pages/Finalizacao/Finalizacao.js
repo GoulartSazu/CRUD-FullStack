1432a09209fb5bab20b0b6f901434eea868b4ef7
Matheus Goulart <goulart77matheus@gmail.com>
2024-06-22 00:48:39 -0300

    Vídeos

--- frontend/src/Pages/Finalizacao/Finalizacao.js
+++ frontend/src/Pages/Finalizacao/Finalizacao.js
@@ -85,23 +85,24 @@ function Finalizacao() {
           >
             <button>
               <div className="left"></div>
-               CANCELAR AGENDAMENTO VIA WHATSAPP ❌
-              <div className="right"></div>
+              CANCELAR AGENDAMENTO VIA WHATSAPP ❌<div className="right"></div>
             </button>
           </Link>
           <div className="videos">
-            {/* <video muted autoPlay="autoplay" loop>
-              <source
-                src="http://fleming.mapa360.com.br/wp-content/uploads/2024/03/copy_BFC2B67E-ABF3-48B8-B80E-1CEC3E91E9C3.mp4"
-                type="video/mp4"
-              />
-            </video>
-            <video muted autoPlay="autoplay" loop>
-              <source
-                src="http://fleming.mapa360.com.br/wp-content/uploads/2024/03/copy_4E66255D-E4E9-4C68-B5D4-9518B4C3A12D.mp4"
-                type="video/mp4"
-              />
-            </video> */}
+            <div className="videos">
+              <video muted autoPlay="autoplay" loop>
+                <source
+                  src="https://www.tratorcase.com.br/wp-content/uploads/2024/06/ferrari.mp4"
+                  type="video/mp4"
+                />
+              </video>
+              <video muted autoPlay="autoplay" loop>
+                <source
+                  src="https://www.tratorcase.com.br/wp-content/uploads/2024/06/proshe.mp4"
+                  type="video/mp4"
+                />
+              </video>
+            </div>
           </div>
         </Finish>
       </Container>
--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { HomePage } from "./Styles";
 import { Container } from "../../styles/global";
 import { Link } from "react-router-dom";
+import video from "../../images/video1.mp4";
 
 function Home() {
   const [title, setTitle] = useState("");
@@ -36,18 +37,18 @@ function Home() {
             </button>
           </Link>
           <div className="videos">
-            {/* <video muted autoPlay="autoplay" loop>
+            <video muted autoPlay="autoplay" loop>
               <source
-                src="http://fleming.mapa360.com.br/wp-content/uploads/2024/03/copy_BFC2B67E-ABF3-48B8-B80E-1CEC3E91E9C3.mp4"
+                src="https://www.tratorcase.com.br/wp-content/uploads/2024/06/ferrari.mp4"
                 type="video/mp4"
               />
             </video>
             <video muted autoPlay="autoplay" loop>
               <source
-                src="http://fleming.mapa360.com.br/wp-content/uploads/2024/03/copy_4E66255D-E4E9-4C68-B5D4-9518B4C3A12D.mp4"
+                src="https://www.tratorcase.com.br/wp-content/uploads/2024/06/proshe.mp4"
                 type="video/mp4"
               />
-            </video> */}
+            </video>
           </div>
         </HomePage>
       </Container>
