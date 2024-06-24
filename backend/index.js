c6a61b84dee499b48ba598641e64e9c9e3c3cdd4
Matheus Goulart <goulart77matheus@gmail.com>
2024-06-23 21:09:02 -0300

    Log

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,15 @@ app.use("/api/agendamento", agendamentoRoutes);
 app.use("/api/veiculo", veiculoRoutes);
 app.use("/api/feedback", feedbackRoutes);
 
+// Verificar a conexão com o banco de dados
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database: ", err);
+    return;
+  }
+  console.log("Connected to the database.");
+});
+
 app.listen(8800, () => {
   console.log("Server is running on port 8800");
 });
