48ca6d0d3a7da1a61bade7b1809ed8fb01f903ff
Matheus Goulart <goulart77matheus@gmail.com>
2024-06-24 16:45:48 -0300

    Banco

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,14 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'splash-admin',
-    password: 'sazu',
-    database: 'splash',
-    charset: 'utf8mb4',
-    port: 3306
+    // host: 'localhost',
+    // user: 'splash-admin',
+    // password: 'sazu',
+    // database: 'splash',
+    // charset: 'utf8mb4',
+    // port: 3306
+    host: "127.0.0.1",
+    user: "splash-admin",
+    password: "sazu",
+    database: "splash"
 })
